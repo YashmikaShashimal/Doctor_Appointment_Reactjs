724902f061215f2a66fc89543d413d727ae6bf23
yashmika <yashmikashashimal07603@gmail.com>
2025-03-21 22:32:38 +0530

    error

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import { assets } from '../assets/assets' // Add this import
 
 const Appointment = () => {
-  return (
+
+  const { docId } = useParams()
+  const { doctors } = useContext(AppContext)
+
+  const [docInfo, setDocInfo] = useState(null)
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find(doc => doc._id === docId)
+    setDocInfo(docInfo)
+  }
+
+  useEffect(()=> {
+    fetchDocInfo()
+  },[doctors,docId])
+
+  return docInfo && (
     <div>
-      
+      {/*------Doctor details-----*/}
+      <div className='flex flex-col gap-4 sm:flex-row'>
+        <div>
+          <img className='w-full rounded-lg bg-primary sm:max-w-72' src={docInfo.image} />
+        </div>
+        <div className='flex-1 border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-[-80px] sm:mt-0'>
+          {/*----- Doc Info : name, degree, experience-----*/}
+          <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>
+            {docInfo.name}
+            <img className='w-5' src={assets.verified_icon} />
+          </p>
+          <div className='flex items-center gap-2 mt-1 text-sm text-gray-600'>
+            <p>{docInfo.degree} - {docInfo.speciality}</p>
+            <button className='py-0.5 px-2 border text-xs rounded-full'>{docInfo.experience}</button>
+          </div>
+         {/*------ Doctor about-------*/}
+          <div>
+            <p className='flex items-center gap-1 mt-3 text-sm font-medium text-gray-900'>About <img src={assets.info_icon} /></p>
+            <p className='text-sm text-gray-500 max-w-[700px] mt-1'>{docInfo.about}</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { AppContext } from '../context/AppContextt'
+import { AppContext } from '../context/AppContext'
 
 const Doctors = () => {
 
