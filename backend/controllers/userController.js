761ee8f68f59f893d721b923fd9382f289d7df11
yashmika <yashmikashashimal07603@gmail.com>
2025-04-07 22:54:28 +0530

    create my profile folder and save it data to database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
+import {v2 as cloudinary} from 'cloudinary'
 
 // API to register user
 
@@ -64,8 +65,60 @@ const loginUser = async (req, res) => {
       res.json({ success: false, message: "Invalid Credentials" });
     }
   } catch (error) {
+    console.log(error)
     res.json({ success: false, message: error.message });
   }
 };
 
-export { registerUser, loginUser };
+// API to get user profile data
+
+const getProfile = async (req,res) => {
+
+  try {
+    
+    const { userId } = req.body
+    const userData = await userModel.findById(userId).select('-password')
+
+    res.json({success:true,userData})
+
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message });
+  }
+
+}
+
+// API to update user profile
+
+const updateProfile = async (req,res) => {
+
+  try {
+    
+    const { userId, name, phone, address, dob, gender } = req.body
+    const imageFile = req.file
+
+    if (!name || !phone || !dob || !gender) {
+      return res.json({ success: false, message: "Missing Details" });
+    }
+
+    await userModel.findByIdAndUpdate(userId,{name,phone,address:JSON.parse(address),dob,gender})
+
+    if (imageFile) {
+      
+      // upload image to cloudnary
+      const imageUpload = await cloudinary.uploader.upload(imageFile.path,{resource_type:'image'})
+      const imageUrl = imageUpload.secure_url
+      
+      await userModel.findByIdAndUpdate(userId,{image:imageUrl})
+    }
+
+    res.json({success:true,message:"Profile Updated"})
+
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message });
+  }
+
+}
+
+export { registerUser, loginUser, getProfile, updateProfile };
