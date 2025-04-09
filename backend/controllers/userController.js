d7c01747f28579816c01254e08328599c3bf5ec5
yashmika <yashmikashashimal07603@gmail.com>
2025-04-09 10:44:44 +0530

    my appointment buttons and it will works updates

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -183,4 +183,61 @@ const bookAppointment = async (req,res) => {
 
 }
 
-export { registerUser, loginUser, getProfile, updateProfile, bookAppointment };
+// API to get user appointments for frontend my-appointment page
+
+const listAppointment = async(req,res) => {
+
+  try {
+    
+    const {userId} = req.body
+    const appointments = await appointmentModel.find({userId})
+
+    res.json({success:true,appointments}) // Fixed typo here
+
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message });
+  }
+
+}
+
+// API to cancel appointment
+
+const cancelAppointment = async (req,res) => {
+
+  try {
+    
+    const {userId, appointmentId} = req.body
+
+    const appointmentData = await appointmentModel.findById(appointmentId)
+
+    // verify appointement user
+
+    if (appointmentData.userId !== userId) {
+      return res.json({success:false,message:'Unauthorized action'})
+    }
+
+    await appointmentModel.findByIdAndUpdate(appointmentId, {cancelled:true})
+
+    // releasing doctors slots
+
+    const {docId, slotDate, slotTime} = appointmentData
+
+    const DoctorData = await doctorModel.findById(docId)
+
+    let slots_booked = DoctorData.slots_booked
+
+    slots_booked[slotDate] = slots_booked[slotDate].filter(e => e !== slotTime)
+
+    await doctorModel.findByIdAndUpdate(docId, {slots_booked})
+
+    res.json({success:true,message:'Appointment Cancelled'})
+
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message });
+  }
+
+}
+
+export { registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment };
--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,31 +1,85 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const MyAppointments = () => {
 
-  const { doctors } = useContext(AppContext)
+  const { backendUrl, token, getDoctorsData } = useContext(AppContext)
+
+  const [appointments,setAppointments] = useState([])
+  const months = [" ", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+  const slotDateFormat = (slotDate) => {
+    const dateArray = slotDate.split('_')
+    return dateArray[0]+ " " + months[Number(dateArray[1])] + " " + dateArray[2]
+  }
+
+  const getUserAppointmets = async() => {
+    try {
+      
+      const {data} = await axios.get(backendUrl + '/api/user/my-appointments',{headers:{token}})
+      
+      if (data.success) {
+        setAppointments(data.appointments.reverse())
+        console.log(data.appointments)
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+    }
+  }
+
+  const cancelAppointment = async(appointmentId) => {
+
+    try {
+      
+      const {data} = await axios.post(backendUrl + '/api/user/cancel-appointment', {appointmentId},{headers:{token}})
+
+      if (data.success) {
+        toast.success(data.message)
+        getUserAppointmets()
+        getDoctorsData()
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+    }
+
+  }
+
+  useEffect (()=> {
+    if (token) {
+      getUserAppointmets()
+    }
+  },[token])
 
   return (
     <div>
       <p className='pb-3 mt-12 font-medium border-b text-zinc-700'>My appointment</p>
       <div>
-        {doctors.slice(0,3).map((item,index)=>(
+        {appointments.map((item,index)=>(
           <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
             <div>
-              <img className='w-32 bg-indigo-50' src={item.image} />
+              <img className='w-32 bg-indigo-50' src={item.docData.image} />
             </div>
             <div className='flex-1 text-sm text-zinc-600'>
-              <p className='font-semibold text-neutral-800'>{item.name}</p>
-              <p>{item.speciality}</p>
+              <p className='font-semibold text-neutral-800'>{item.docData.name}</p>
+              <p>{item.docData.speciality}</p>
               <p className='mt-1 font-medium text-zinc-700'>Address:</p>
-              <p className='text-xs'>{item.address.line1}</p>
-              <p className='text-xs'>{item.address.line2}</p>
-              <p className='mt-1 text-xs'><span className='text-sm font-medium text-neutral-700'>Date & Time:</span> 25, July, 2025 | 8:30 PM</p>
+              <p className='text-xs'>{item.docData.address.line1}</p>
+              <p className='text-xs'>{item.docData.address.line2}</p>
+              <p className='mt-1 text-xs'><span className='text-sm font-medium text-neutral-700'>Date & Time:</span> {slotDateFormat(item.slotDate)} | {item.slotTime}</p>
             </div>
             <div></div>
             <div className='flex flex-col justify-end gap-2'>
-              <button className='py-2 text-sm text-center transition-all duration-300 border rounded text-stone-500 sm:min-w-48 hover:bg-primary hover:text-white'>Pay Online</button>
-              <button className='py-2 text-sm text-center transition-all duration-300 border rounded text-stone-500 sm:min-w-48 hover:bg-red-600 hover:text-white'>Cancel appointment</button>
+              {!item.cancelled && <button className='py-2 text-sm text-center transition-all duration-300 border rounded text-stone-500 sm:min-w-48 hover:bg-primary hover:text-white'>Pay Online</button>}
+              {!item.cancelled && <button onClick={()=>cancelAppointment(item._id)} className='py-2 text-sm text-center transition-all duration-300 border rounded text-stone-500 sm:min-w-48 hover:bg-red-600 hover:text-white'>Cancel appointment</button>}
+              {item.cancelled && <button className='py-2 border border-red-500 rounded sm:min-w-48'>Appointment cancelled</button>}
             </div>
           </div>
         ))}
