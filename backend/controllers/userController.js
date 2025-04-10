434010f52610cba0763ebc1a04563053887e2e48
yashmika <yashmikashashimal07603@gmail.com>
2025-04-10 17:40:12 +0530

    payment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,7 @@ import jwt from "jsonwebtoken";
 import {v2 as cloudinary} from 'cloudinary'
 import doctorModel from "../models/doctorModel.js";
 import appointmentModel from "../models/appointmentModel.js";
+import paypal from "@paypal/react-paypal-js"
 
 // API to register user
 
@@ -240,4 +241,46 @@ const cancelAppointment = async (req,res) => {
 
 }
 
-export { registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment };
+const paypalInstance = new paypal({
+  key_id: process.env.PAYPAL_KEY_ID,
+  key_secret: process.env.PAYPAL_KEY_SECRET
+})
+
+// API to make payment of appointment using razorpay
+
+const paymentPaypal = async(req,res) => {
+
+  try {
+      
+      const {appointmentId} = req.body
+    const appointmentData = await appointmentModel.findById(appointmentId)
+
+    if (!appointmentData || appointmentData.cancelled) {
+      return res.json({success:false,message:"Appointment Cancelled or not found"})
+    }
+
+    // creating optins for paypal payment
+
+    const options = {
+      amount: appointmentData.amount * 100,
+      currency: process.env.CURRENCY,
+      receipt: appointmentId,
+    }
+
+    // creations of an order
+
+    const order = await paypalInstance.oders.create(options)
+
+    res.json({success:true,order})
+
+
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message });
+  }
+
+  
+
+}
+
+export { registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment, paymentPaypal };
