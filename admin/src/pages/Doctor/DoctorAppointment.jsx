b1cb118410887cbe61b26230dc193686e1fd76f6
yashmika <yashmikashashimal07603@gmail.com>
2025-04-18 13:04:58 +0530

    appoint panel done

--- admin/src/pages/Doctor/DoctorAppointment.jsx
+++ admin/src/pages/Doctor/DoctorAppointment.jsx
@@ -30,7 +30,7 @@ const DoctorAppointment = () => {
           <p>Action</p>
         </div>
         {
-          appointments.map((item,index)=> (
+          appointments.reverse().map((item,index)=> (
             <div className='flex flex-wrap justify-between max-sm:gap-5 max-sm:text-base sm:grid grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50' key={index}>
               <p className='max-sm:hidden'>{index + 1}</p>
               <div className='flex items-center gap-2'>
@@ -44,10 +44,16 @@ const DoctorAppointment = () => {
               <p className='max-sm:hidden'>{calculateAge(item.userData.dob)}</p>
               <p>{slotDateFormat(item.slotDate)}, {item.slotTime}</p>
               <p>{currency} {item.amount}</p>
-              <div className='flex'>
-                <img onClick={()=>cancelAppointment(item._id)} className='w-10 cursor-pointer hover:scale-105' src={assets.cancel_icon} />
-                <img onClick={()=>completeAppointment(item._id)} className='w-10 cursor-pointer hover:scale-105' src={assets.tick_icon} />
-              </div>
+              {
+                item.cancelled 
+                ? <p className='text-xs font-medium text-red-400'>cancelled</p>
+                : item.isCompleted
+                  ? <p className='text-xs font-medium text-green-500'>Completed</p>
+                  : <div className='flex'>
+                      <img onClick={()=>cancelAppointment(item._id)} className='w-10 cursor-pointer hover:scale-105' src={assets.cancel_icon} />
+                      <img onClick={()=>completeAppointment(item._id)} className='w-10 cursor-pointer hover:scale-105' src={assets.tick_icon} />
+                    </div>
+              }
             </div>
           ))
         }
