bbe4c41b1113c27d8de85a2306446357aa605ae3
yashmika <yashmikashashimal07603@gmail.com>
2025-04-21 15:41:11 +0530

    logo update

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between px-4 py-3 bg-white border-b sm:px-10'>
       <div className='flex items-center gap-2 text-xs'>
-        <img className='cursor-pointer w-36 sm:w-40' src={assets.admin_logo} />
+        <img className='cursor-pointer w-60 sm:w-44' src={assets.admin_logo} />
         <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
       </div>
       <button onClick={logout} className='px-10 py-2 text-sm text-white rounded-full bg-primary'>Logout</button>
