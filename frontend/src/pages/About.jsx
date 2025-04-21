6ef6316e94e47d228bc6fa031ff63948f51bc669
yashmika <yashmikashashimal07603@gmail.com>
2025-04-21 15:43:22 +0530

    text edit

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -10,10 +10,10 @@ const About = () => {
       <div className='flex flex-col gap-12 my-10 md:flex-row'>
         <img className='w-full md:max-w-[360px]' src={assets.about_image} />
         <div className='flex flex-col justify-center gap-6 text-sm text-gray-600 md:w-2/4'>
-          <p>Welcome to Prescripto, Your trusted partner in managing your healthcare needs conveniently and efficiently.At Prescripto, We understand the challanges indivituals face when it comes to schrduling doctor appointment and managing their health record.</p>
-          <p>Prescripto is Commited to excellence in healthcare technology.We Continuodly dtrive to enhance our flatform, Intergrating the laytest advancements to improve user experience and deliver superior service.Whether You're Booking Your first appointment or managinf ongooing care, Prescripto is here to support you every step of the day.</p>
+          <p>Welcome to E-Channeling, Your trusted partner in managing your healthcare needs conveniently and efficiently.At Prescripto, We understand the challanges indivituals face when it comes to schrduling doctor appointment and managing their health record.</p>
+          <p>E-Channeling is Commited to excellence in healthcare technology.We Continuodly dtrive to enhance our flatform, Intergrating the laytest advancements to improve user experience and deliver superior service.Whether You're Booking Your first appointment or managinf ongooing care, E-Channeling is here to support you every step of the day.</p>
           <b className='text-gray-800'>Our Vision</b>
-          <p>Our Vision at Prescripto is to create a seamiess healthcare ecperience for every user.We aim to bridge the Gop between patients and healthcare providers.Making it easier for you to access the care you need,When You Need it.</p>
+          <p>Our Vision at E-Channeling is to create a seamiess healthcare ecperience for every user.We aim to bridge the Gop between patients and healthcare providers.Making it easier for you to access the care you need,When You Need it.</p>
         </div>
       </div>
       <div className='my-4 text-xl'>
