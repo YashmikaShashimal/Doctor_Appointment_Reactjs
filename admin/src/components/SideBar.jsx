48ae288dae69fcada429c6ffd59f53a47c96757c
yashmika <yashmikashashimal07603@gmail.com>
2025-04-22 20:24:27 +0530

    ok

--- admin/src/components/SideBar.jsx
+++ admin/src/components/SideBar.jsx
@@ -13,19 +13,19 @@ const SideBar = () => {
     <div className='min-h-screen bg-white border-r'>
       {
         aToken && <ul className='text-[#515151] mt-5'>
-          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`} to={'/admin-dashboard'}>
+          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer hover:scale-105 hover:text-primary ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary text-primary' : ''}`} to={'/admin-dashboard'}>
             <img src={assets.home_icon} />
             <p className='hidden md:block'>Dashboard</p>
           </NavLink>
-          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}  to={'/all-appointments'}>
+          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer hover:scale-105 hover:text-primary ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary text-primary' : ''}`}  to={'/all-appointments'}>
             <img src={assets.appointment_icon} />
             <p className='hidden md:block'>Appointments</p>
           </NavLink>
-          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`} to={'/add-doctor'}>
+          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer hover:scale-105 hover:text-primary ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary text-primary' : ''}`} to={'/add-doctor'}>
             <img src={assets.add_icon} />
             <p className='hidden md:block'>Add Doctor</p>
           </NavLink>
-          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`} to={'/doctor-list'}>
+          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer hover:scale-105 hover:text-primary ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary text-primary' : ''}`} to={'/doctor-list'}>
             <img src={assets.people_icon} />
             <p className='hidden md:block'>Doctors List</p>
           </NavLink>
@@ -33,16 +33,16 @@ const SideBar = () => {
       }
       {
         dToken && <ul className='text-[#515151] mt-5'>
-          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`} to={'/doctor-dashboard'}>
+          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer hover:scale-105 hover:text-primary ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary text-primary' : ''}`} to={'/doctor-dashboard'}>
             <img src={assets.home_icon} />
             <p className='hidden md:block'>Dashboard</p>
           </NavLink>
-          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}  to={'/doctor-appointments'}>
+          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer hover:scale-105 hover:text-primary ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary text-primary' : ''}`}  to={'/doctor-appointments'}>
             <img src={assets.appointment_icon} />
             <p className='hidden md:block'>Appointments</p>
           </NavLink>
           
-          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`} to={'/doctor-profile'}>
+          <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer hover:scale-105 hover:text-primary ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary text-primary' : ''}`} to={'/doctor-profile'}>
             <img src={assets.people_icon} />
             <p className='hidden md:block'>Profile</p>
           </NavLink>
